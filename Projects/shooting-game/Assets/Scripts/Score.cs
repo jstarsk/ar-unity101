0fe1347cab04278b533a9ab9d2e076400b262ac1
Starsky Lara <starsky@Joses-MacBook-Pro.local>
2018-03-08 16:25:40 +0100

    13

---
+++ Projects/shooting-game/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+        score = score + 1;
+        print("HIT!!!!!!!, SOCORE =" + score);
+	}
+}
