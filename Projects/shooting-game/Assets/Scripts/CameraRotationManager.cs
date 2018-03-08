3c76ce77dea991e7ad83983756757cf464ab836a
Starsky Lara <starsky@Joses-MacBook-Pro.local>
2018-03-08 15:32:07 +0100

    10

---
+++ Projects/shooting-game/Assets/Scripts/CameraRotationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotationManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float rotationSpeed = 5.0f;
+        float xMouse = Input.GetAxis("Mouse X");;
+        float yMouse = Input.GetAxis("Mouse Y"); ;
+        print("X: " + xMouse + " Y: " + yMouse);
+	}
+}
